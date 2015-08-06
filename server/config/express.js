8089bc61c88e149c9ab079daac91993d45653526
Philip Riley <hiriley510@gmail.com>
2015-08-05 18:57:21 -0700

    favicon stuff

--- server/config/express.js
+++ server/config/express.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var express = require('express');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
@@ -28,7 +28,7 @@ module.exports = function(app) {
   app.use(cookieParser());
 
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    // app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
