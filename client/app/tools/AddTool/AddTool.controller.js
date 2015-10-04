b7204a90ca40213debf2356576a75169dd56fae0
Daniel Rasmuson <dan123911@gmail.com>
2015-10-04 14:49:01 -0700

    added add tool

---
+++ client/app/tools/AddTool/AddTool.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('civicMakersClientApp')
+  .controller('AddToolCtrl', function ($scope, $routeParams, ToolApi) {
+    console.log('ctrl is being initilaized');
+  	// console.log("routeParams",$routeParams)
+    // $scope.tool = ToolApi.queryTool($routeParams.toolID);
+  });
