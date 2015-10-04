470adb4735ec15906b90b5575ff1ce345fee2073
Daniel Rasmuson <dan123911@gmail.com>
2015-10-04 15:56:21 -0700

    have the form validation working

--- client/app/tools/AddTool/AddTool.controller.js
+++ client/app/tools/AddTool/AddTool.controller.js
@@ -2,7 +2,15 @@
 
 angular.module('civicMakersClientApp')
   .controller('AddToolCtrl', function ($scope, $routeParams, ToolApi) {
-    console.log('ctrl is being initilaized');
+    $scope.toolFormData = {};
+    $scope.submitToolForm = function(form){
+      if ($scope.toolForm.$valid){
+        console.log(form);    
+        // window.location.replace("/")
+      } else{
+        alert('the form is not valid');
+      }
+    }
   	// console.log("routeParams",$routeParams)
     // $scope.tool = ToolApi.queryTool($routeParams.toolID);
   });
