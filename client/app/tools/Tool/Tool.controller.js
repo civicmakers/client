f7a6d08071a3a9763558e8aaeeb145f265f24e77
Lawrence Grodeska <LSG@Lawrences-MacBook-Air.local>
2015-11-08 16:19:33 -0800

    updated and styled card directive

--- client/app/tools/Tool/Tool.controller.js
+++ client/app/tools/Tool/Tool.controller.js
@@ -3,8 +3,10 @@
 angular.module('civicMakersClientApp')
   .controller('ToolCtrl', function ($scope, $routeParams, ToolApi) {
 
+  	 console.log("routeParams", $routeParams);
+
     ToolApi.queryTool($routeParams.toolID).then(function (tool){
-    	console.log(tool)
+    	console.log('tool',tool)
       $scope.tool = tool
     })
 
